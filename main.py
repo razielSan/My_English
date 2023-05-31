6df8d39b8523f5bd5b9d0e8b0f49d8eb69e884cc
raziel <tvindzond@gmail.com>
2023-05-31 13:29:22 +0700

    message 4

--- main.py
+++ main.py
@@ -4,11 +4,38 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+lst_main_menu = [
+    {'name': 'Главная', 'url': '/'},
+    {'name': 'Добавить', 'url': '/add'},
+    {'name': 'Обновить', 'url': '/update'},
+    {'name': 'Удалить', 'url': '/delete'},
+    {'name': 'Show', 'url': '/show'},
+]
+ 
+
 @app.route('/')
 def index():
-    return render_template('index.html', name='Tina')
+    return render_template('index.html', title='Главная', lst_main_menu=lst_main_menu)
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html', title='Добавление', lst_main_menu=lst_main_menu)
+
+
+@app.route('/update')
+def update():
+    return render_template('update.html', title='Просмотр', lst_main_menu=lst_main_menu)
+
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html', title='Удаление', lst_main_menu=lst_main_menu)
+
+@app.route('/show')
+def show():
+    return render_template('show.html', title='Просмотр', lst_main_menu=lst_main_menu)
 
 
 if __name__ == '__main__':
     app.run(debug=1)
-
